3ae907bcad8f1a061efc970f745375e5631441be
Rohan Ramachandran <rmramachandr@wisc.edu>
2022-03-06 22:30:33 -0600

    plays in-line

--- client/src/components/HeroSection/index.js
+++ client/src/components/HeroSection/index.js
@@ -14,7 +14,7 @@ const HeroSection = () => {
   return (
     <HeroContainer id='home'>
         <HeroBg>
-            <VideoBg autoPlay loop muted src={Video} type='video/mp4' />
+            <VideoBg playsInline autoPlay loop muted src={Video} type='video/mp4' />
 
 
         </HeroBg>
