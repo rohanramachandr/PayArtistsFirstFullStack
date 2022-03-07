e4511d7dde2e11a1eeddc6da6f4c259bc91d347f
Rohan Ramachandran <rmramachandr@wisc.edu>
2022-03-06 21:32:14 -0600

    finished connecting frontend and server for auth

--- index.js
+++ index.js
@@ -22,6 +22,19 @@ app.use(passport.session());
 
 require('./routes/authRoutes')(app);
 
+if (process.env.NODE_ENV === 'production') {
+    //express will serve up production assets 
+    //main.js or main.css
+    app.use(express.static('client/build'));
+
+    //express will serve up index.htmlfile if it doesn't recognize route
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname), 'client', 'build', 'index.html');
+    });
+
+}
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT);
