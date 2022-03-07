ef2b132443765d09e624e832f94ad8b3e64b30fa
Rohan Ramachandran <rmramachandr@wisc.edu>
2022-03-06 22:15:50 -0600

    retrying deploy

--- index.js
+++ index.js
@@ -23,17 +23,17 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 
 if (process.env.NODE_ENV === 'production') {
-    //express will serve up production assets 
-    //main.js or main.css
+    // Express will serve up production assets
+    // like our main.js file, or main.css file!
     app.use(express.static('client/build'));
-
-    //express will serve up index.htmlfile if it doesn't recognize route
+  
+    // Express will serve up the index.html file
+    // if it doesn't recognize the route
     const path = require('path');
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname), 'client', 'build', 'index.html');
+      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
-
-}
+  }
 
 const PORT = process.env.PORT || 5000;
 
