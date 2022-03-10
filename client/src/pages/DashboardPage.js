efdd4bbadf715eeed1fc5513979fabb40261c518
Rohan Ramachandran <rmramachandr@wisc.edu>
2022-03-10 09:19:04 -0600

    finished dashboard navbar

--- client/src/pages/DashboardPage.js
+++ client/src/pages/DashboardPage.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import NowPlayingBar from '../components/Dashboard/NowPlayingBar';
+import { BsSearch as Search } from 'react-icons/bs'; 
+import './DashboardPage.css';
 
 
 const Dashboard = (props) => {
@@ -22,15 +24,77 @@ const Dashboard = (props) => {
 
 
     return (
-        <>
-            
-
-            
-           <NowPlayingBar />
 
 
+        <div id="mainContainer">
 
-        </>
+
+            <div id="topContainer">
+
+                <div id="navBarContainer">
+                    <nav className="navBar">
+                        <a href="/dashboard" className="logo">PayArtistsFirst</a>
+
+                        <div className="group">
+
+                            <div className="navItem">
+
+                                <a href="/search" className="navItemLink">Search
+
+                                    <Search id="searchIcon" size={20} alt="Shuffle" />
+                                </a>
+
+                            </div>
+
+                        </div>
+
+                        <div className="group">
+
+                            <div className="navItem">
+
+                                <a href="/browse" className="navItemLink">Browse</a>
+
+                            </div>
+
+                            <div className="navItem">
+
+                                <a href="/yourmusic" className="navItemLink">Your Music</a>
+
+                            </div>
+
+                            <div className="navItem">
+
+                                <a href="/profile" className="navItemLink">Rohan Ramachandran</a>
+
+                            </div>
+
+                            <div className="navItem">
+
+                                <a href="/api/logout" className="navItemLink">Log Out</a>
+
+                            </div>
+
+                        </div>
+
+                    </nav>
+
+
+                </div>
+
+            </div>
+
+            <NowPlayingBar />
+
+
+        </div>
+
+
+
+
+
+
+
+
     );
 };
 
