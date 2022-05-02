53e4aece8026c9e39217c3379662d5ec2d32af67
Rohan Ramachandran <rmramachandr@wisc.edu>
2022-05-02 10:35:23 -0500

    got responsive nav bar to display

--- client/src/pages/DashboardPage.js
+++ client/src/pages/DashboardPage.js
@@ -10,6 +10,7 @@ import theme from "../components/Dashboard/theme";
 import { ThemeProvider } from "@material-ui/core";
 import SwipeableNavBar from "../components/Dashboard/DashboardNav/SwipeableNav";
 import { DashboardContextProvider } from "../components/Dashboard/DashboardContext";
+import ResponsivePlayingBar from "../components/Dashboard/NowPlayingBar/ResponsivePlayingBar";
 
 const DashboardPage = (props) => {
 
@@ -23,7 +24,8 @@ const DashboardPage = (props) => {
           <div id="topContainer">
             <Outlet />
           </div>
-          <NowPlayingBar />
+          {/* <NowPlayingBar /> */}
+          <ResponsivePlayingBar />
         </div>
       </DashboardContextProvider>
     </ThemeProvider>
