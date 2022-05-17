bb1e2b16f9f3a2cbed5c430238930776962de7d3
Rohan Ramachandran <rmramachandr@wisc.edu>
2022-05-17 10:47:26 -0500

    made become artist form responsive

--- client/src/components/Dashboard/BecomeArtist/BecomeArtistModal.js
+++ client/src/components/Dashboard/BecomeArtist/BecomeArtistModal.js
@@ -1,5 +1,6 @@
-import React, {useContext} from 'react';
-import {Box, Modal, Fade, Typography, Backdrop, TextField, Button} from '@material-ui/core';
+import React, { useContext } from 'react';
+import { Box, Modal, Fade, Typography, Backdrop, TextField, Button, Grid, IconButton } from '@material-ui/core';
+import CloseIcon from '@mui/icons-material/Close';
 import { DashboardContext } from '../DashboardContext';
 
 const style = {
@@ -7,26 +8,34 @@ const style = {
     top: '50%',
     left: '50%',
     transform: 'translate(-50%, -50%)',
-    width: 500,
     height: 400,
+    width: 350,
+    '@media screen and (max-width: 820px)': {
+        width: '95%'
+    },
     bgcolor: 'background.paper',
     border: '2px solid #000',
     boxShadow: 24,
-    p: 4,
+    p: 4
 };
 
-const submitButtonStyles = {
+const textFieldStyle = {
+    width: '100%',
+    margin: '5% 10%'
 
-    position: "absolute",
-    bottom: "10px",
-    right: "10px",
-    
+};
 
+
+
+const closeIconStyle = {
+    position: 'absolute',
+    top: "5px",
+    right: "5px"
 
 };
 
 export default function BecomeArtistModal() {
-    const {becomeArtistOpen, setBecomeArtistOpen} = useContext(DashboardContext);
+    const { becomeArtistOpen, setBecomeArtistOpen } = useContext(DashboardContext);
     const handleClose = () => setBecomeArtistOpen(false);
 
     return (
@@ -45,15 +54,28 @@ export default function BecomeArtistModal() {
         >
             <Fade in={becomeArtistOpen}>
                 <Box sx={style}>
-                    <Typography id="transition-modal-title" variant="h6" component="h2">
-                        Set Up An Artist Profile
-                    </Typography>
-                    <Typography id="transition-modal-description" sx={{ mt: 2 }}>
-                        Please create a unique artist username
-                    </Typography>
-                    <TextField  id="standard-basic" label="Artist Username" variant="standard" color='primary' helperText="This username is already taken" />
-                    <Button style={submitButtonStyles} variant="contained" color="primary">Create Artist Profile</Button>
- 
+                    <IconButton aria-label="delete" style={closeIconStyle} onClick={handleClose}>
+                        <CloseIcon />
+                    </IconButton>
+                    <Grid container
+                        direction="row"
+                        justify="center"
+                        alignItems="center"
+                        style={{marginTop: "15px"}}
+                    >
+                        <Typography id="transition-modal-title" variant="h6" component="h2" fullWidth>
+                            Set Up An Artist Profile
+                        </Typography>
+                        <Typography id="transition-modal-description" sx={{ mt: 10 }} fullWidth>
+                            Create a unique artist username
+                        </Typography>
+                        <TextField id="standard-basic" label="Artist Username" variant="standard" color='primary' helperText="This username is already taken" style={textFieldStyle} />
+                        <Button variant="contained" color="primary" >Create Artist Profile</Button>
+
+                    </Grid>
+
+
+
                 </Box>
             </Fade>
         </Modal>
