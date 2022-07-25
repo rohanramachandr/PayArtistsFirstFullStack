ef02eb1b9663e3cd666da604ef7b04d5da2ae6e8
Rohan Ramachandran <rmramachandr@wisc.edu>
2022-07-25 10:45:37 -0500

    fixed heroku private key bug

--- config/prod.js
+++ config/prod.js
@@ -11,6 +11,6 @@ module.exports = {
     musicBucketName: process.env.MUSIC_BUCKET_NAME,
     cloudfrontUrl: process.env.CLOUD_FRONT_URL,
     cloudfrontPublicKey: process.env.CLOUD_FRONT_PUBLIC_KEY_ID,
-    cloudfrontPrivateKey: process.env.RSA_PRIVATE_KEY
+    cloudfrontPrivateKey: JSON.parse(process.env.RSA_PRIVATE_KEY)
 
 };
