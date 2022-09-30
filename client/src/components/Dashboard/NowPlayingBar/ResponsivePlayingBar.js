4acf413cb727fd87eb7d4188a9b639602f81f573
Rohan Ramachandran <rmramachandr@wisc.edu>
2022-09-30 16:53:38 -0500

    added load in beginnging of setup Media Session

--- client/src/components/Dashboard/NowPlayingBar/ResponsivePlayingBar.js
+++ client/src/components/Dashboard/NowPlayingBar/ResponsivePlayingBar.js
@@ -89,6 +89,7 @@ const ResponsivePlayingBar = ({
     const setupMediaSessions = useCallback((songInfo, playlist) => {//avoid using state varaible in media sessions
         if ('mediaSession' in navigator) {
             // console.log("navigator setupped");
+            audioPlayer.current.load();
             audioPlayer.current.currentTime = 0;
             navigator.mediaSession.metadata = new window.MediaMetadata({
                 // title: currentVideoSnippet.title,
